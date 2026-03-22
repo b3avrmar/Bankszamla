19da8a395d39635dceedb4a6fd64f7d526ce3213
b3avrmar <b3avrmar@vasvari.org>
2026-03-22 21:29:39 +0100

    Class properties created

--- Bankszamla/Bankszamla/Account.cs
+++ Bankszamla/Bankszamla/Account.cs
@@ -8,5 +8,20 @@ namespace Bankszamla
 {
     internal class Account
     {
+        private string accountNumber;
+        private string ownerName;
+        private decimal balance;
+        private decimal creditLimit;
+
+        private List<string> logs = new List<string>();
+
+        public Account(string accountNumber, string ownerName, decimal openingBalance)
+        {
+            this.accountNumber = accountNumber;
+            this.ownerName = ownerName;
+            this.balance = openingBalance;
+            this.creditLimit = openingBalance * 0.2m;
+        }
+
     }
 }
