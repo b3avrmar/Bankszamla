952bd55f7168bea9b432369a879f809bc5b929c2
b3avrmar <b3avrmar@vasvari.org>
2026-03-22 21:44:14 +0100

    Menu created

--- Bankszamla/Bankszamla/Program.cs
+++ Bankszamla/Bankszamla/Program.cs
@@ -8,8 +8,61 @@ namespace Bankszamla
 {
     internal class Program
     {
+
+        static List<Account> accounts = new List<Account>();
+
         static void Main(string[] args)
         {
+            LoadAccounts();
+
+            bool running = true;
+
+            while (running)
+            {
+                Console.Clear();
+                ShowMenu();
+
+                string choice = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        ShowAccounts();
+                        break;
+
+                    case "2":
+                        MakeDeposit();
+                        break;
+
+                    case "3":
+                        MakeWithdraw();
+                        break;
+
+                    case "4":
+                        MakeTransfer();
+                        break;
+
+                    case "5":
+                        SaveAllLogs();
+                        Console.WriteLine("Naplók elmentve.");
+                        running = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Érvénytelen menüpont.");
+                        break;
+                }
+
+                if (running)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Nyomj meg egy gombot a folytatáshoz...");
+                    Console.ReadKey();
+                }
+            }
         }
+
+        
     }
 }
